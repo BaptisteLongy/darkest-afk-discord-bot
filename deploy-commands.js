0a20beba602e9a077a21d0d53e8eb28b722a35d3
Baptiste Longy <baptiste.longy@gmail.com>
2021-10-06 21:55:05 +0200

    feat: added a deploy-commands script

---
+++ deploy-commands.js
@@ -0,0 +1,22 @@
+const dotenv = require('dotenv').config()
+const fs = require('fs');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+
+const clientId = "895326452365553665"
+const guildId = "406861607390543873"
+
+const commands = [];
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	commands.push(command.data.toJSON());
+}
+
+const rest = new REST({ version: '9' }).setToken(process.env.DISCORD_TOKEN);
+
+// rest.put(Routes.applicationCommands(clientId, guildId), { body: commands })
+rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+	.then(() => console.log('Successfully registered application commands.'))
+	.catch(console.error);
