a79d136b00344b1e1ec6765ee516f27469770972
Baptiste Longy <baptiste.longy@gmail.com>
2021-10-16 13:59:09 +0200

    feat: added /summon gems command

--- src/commands/summon.js
+++ src/commands/summon.js
@@ -1,5 +1,7 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 
+const gemsEmbedColor = "#7a14e1"
+
 const bonuses = [
     {
         "rewards_list": [
@@ -202,33 +204,130 @@ function generateSummonBonusList() {
         bonus.rewards_list.forEach(reward => {
             bonusText += `\n${reward.quantity}x ${reward.name}`
         })
-        
+
         if (finalText === "") {
             return finalText = bonusText
         } else {
             return finalText = finalText + '\n' + bonusText
         }
-    }, "")    
+    }, "")
+}
+
+function generateGemSummonText() {
+    gemText = "**Hero Summon using gems**"
+
+    probabilitiesEmbed = {
+        title: "Probabilities",
+        color: gemsEmbedColor,
+        fields: [
+            {
+                name: "True Hero",
+                value: "16%",
+                inline: true
+            },
+            {
+                name: "Soul",
+                value: "2%",
+                inline: true
+            },
+            {
+                name: "Fallen Hero",
+                value: "82%",
+                inline: true
+            }
+        ]
+    }
+
+    soulUnlockEmbed = {
+        title: "Soul Unlock level",
+        color: gemsEmbedColor,
+        fields: [
+            {
+                name: "Grey Soul",
+                value: "Always",
+                inline: true
+            },
+            {
+                name: "Green Soul",
+                value: "Once Chapter 6 is reached",
+                inline: true
+            },
+            {
+                name: "Blue Soul",
+                value: "Once Chapter 9 is reached",
+                inline: true
+            },
+            {
+                name: "Purple Soul",
+                value: "Once Chapter 12 is reached",
+                inline: true
+            }
+        ]
+    }
+
+    soulWeightEmbed = {
+        title: `Soul Weight (once unlocked)`,
+        description: `Warning - Those are weights, not percentages`,
+        color: gemsEmbedColor,
+        fields: [
+            {
+                name: "Grey Soul",
+                value: "80",
+                inline: true
+            },
+            {
+                name: "Green Soul",
+                value: "40",
+                inline: true
+            },
+            {
+                name: "Blue Soul",
+                value: "20",
+                inline: true
+            },
+            {
+                name: "Purple Soul",
+                value: "10",
+                inline: true
+            }
+        ]
+    }
+
+    let additionalInfoDescription = "A Fallen Hero is guaranteed as your first pull"
+    additionalInfoDescription += "\nA True Hero is guaranteed as your 5th pull"
+    additionalInfoDescription += "\nA Soul is guaranteed as your 11th pull"
+    additionalInfoEmbed = {
+        title: `Additional infos`,
+        description: additionalInfoDescription,
+        color: gemsEmbedColor,
+    }
+
+    return { content: gemText, embeds: [probabilitiesEmbed, soulUnlockEmbed, soulWeightEmbed, additionalInfoEmbed] }
 }
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('summon')
-		.setDescription('Everything we know about Hero Summoning')
+    data: new SlashCommandBuilder()
+        .setName('summon')
+        .setDescription('Everything we know about Hero Summoning')
         .addSubcommand(subCommand =>
-			subCommand.setName('bonus')
-				.setDescription('List all the Hero Summon bonuses in their cycling order')
-		),
-	async execute(interaction) {
+            subCommand.setName('bonus')
+                .setDescription('List all the Hero Summon bonuses in their cycling order')
+        ).addSubcommand(subCommand =>
+            subCommand.setName('gems')
+                .setDescription('List Hero Summon probabilities when using gems')
+        ),
+    async execute(interaction) {
         await interaction.deferReply()
 
-		try {
-			if (interaction.options.getSubcommand() === 'bonus') {
+        try {
+            if (interaction.options.getSubcommand() === 'bonus') {
                 await interaction.editReply("**Hero Summon bonuses in their cycling order** \n" + generateSummonBonusList())
-			}
-		} catch (error) {
-			console.log(error)
-			await interaction.editReply({ content: `Something bad happened :(`, ephemeral: true });
-		}
-	},
+            } else if (interaction.options.getSubcommand() === 'gems') {
+                await interaction.editReply(generateGemSummonText())
+            }
+        } catch (error) {
+            console.log(error)
+            await interaction.editReply({ content: `Something bad happened :(`, ephemeral: true });
+        }
+    },
 };
